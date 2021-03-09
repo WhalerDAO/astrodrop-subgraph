5a4402d06fe88db486c38cba8f1d853e855e60fa
Zefram Lou <zeframlou@gmail.com>
2021-03-08 19:27:08 -0800

    Implement basic features

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,49 +1,46 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Bytes, ipfs, json } from "@graphprotocol/graph-ts"
 import {
-  AstrodropFactory,
   CreateAstrodrop
 } from "../generated/AstrodropFactory/AstrodropFactory"
-import { ExampleEntity } from "../generated/schema"
+import { Claimant, Astrodrop, ClaimantAstrodropRelation } from "../generated/schema"
 
 export function handleCreateAstrodrop(event: CreateAstrodrop): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  // fetch IPFS file
+  let truncatedIPFSHash = event.params.ipfsHash
+  let ipfsHash = new Bytes(34)
+  ipfsHash[0] = 0x12;
+  ipfsHash[1] = 0x20;
+  for (let i = 0; i < 32; i++) {
+    ipfsHash[i + 2] = truncatedIPFSHash[i]
   }
+  let ipfsHashString = ipfsHash.toBase58()
+  let rawRootFile = ipfs.cat(ipfsHashString)
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // create Astrodrop entity
+  let astrodrop = new Astrodrop(event.params.astrodrop.toHex());
+  astrodrop.ipfsHash = ipfsHashString;
+  astrodrop.save()
 
-  // Entity fields can be set based on event parameters
-  entity.astrodrop = event.params.astrodrop
-  entity.ipfsHash = event.params.ipfsHash
+  // parse IPFS file
+  if (rawRootFile != null) {
+    let rootFile = json.fromBytes(rawRootFile!).toObject()
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+    // parse claimants
+    let claimants = rootFile.get('keys').toArray()
+    for (let i = 0; i < claimants.length; i++) {
+      // create Claimant entity
+      let claimant = claimants[i].toString()
+      let claimantEntity = Claimant.load(claimant)
+      if (claimantEntity == null) {
+        claimantEntity = new Claimant(claimant)
+        claimantEntity.save()
+      }
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.computeAstrodropAddress(...)
-  // - contract.createAstrodrop(...)
+      // create ClaimantAstrodropRelation entity
+      let claimantAstrodropRelation = new ClaimantAstrodropRelation(claimant + astrodrop.id)
+      claimantAstrodropRelation.claimant = claimant
+      claimantAstrodropRelation.astrodrop = astrodrop.id
+      claimantAstrodropRelation.save()
+    }
+  }
 }
